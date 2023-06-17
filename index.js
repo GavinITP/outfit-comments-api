22de04bd92a857981c0ed65b922edb1ef74c53f9
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-06-17 20:12:33 +0700

    get data in outfit endpoint

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ const { v4: uuid } = require("uuid");
 const app = express();
 
 app.get("/outfit", (req, res) => {
-  res.send("working!!");
+  const colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "black",
+    "white",
+    "gray",
+  ];
+
+  res.json({
+    top: _.sample(colors),
+    pants: _.sample(colors),
+    shoes: _.sample(colors),
+  });
 });
 
 app.listen(3000, () => console.log("running"));
